75a8cff8939fcf2d86754a736f16f63a376657ac
ProxyTechDevs <44120635+ProxyTechDevs@users.noreply.github.com>
2018-10-13 14:33:33 -0400

    Create EventListener.php

---
+++ src/ProxyTechDevs/CustomMobDropsPMMP/EventListener.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace ProxyTechDevs\CustomMobDropsPMMP;
+use pocketmine\entity\Human;
+use pocketmine\entity\Living;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+
+class EventListener implements Listener {
+	
+	/** @var Main */
+	public $plugin;
+	
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	public function onEntityDeath(EntityDeathEvent $ev){
+		if(!($ev->getEntity() instanceof Human) && $ev->getEntity() instanceof Living){
+			if(isset($this->plugin->config->getAll()[$ev->getEntity()::NETWORK_ID])){
+				$arr = [];
+				foreach($this->plugin->config->getAll()[$ev->getEntity()::NETWORK_ID] as $itemArr){
+					$arr[] = Item::get($itemArr[0], $itemArr[1], $itemArr[2]);
+				}
+				$ev->setDrops($arr);
+			}
+		}
+	}
+}
